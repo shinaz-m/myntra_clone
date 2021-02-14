a399cb99bf85e259aed2443c09889c4b96b24753
Shinazm <mohammad.shinaz@nineleaps.com>
2021-02-15 02:18:48 +0530

    Feature| created search feature for product

--- controllers/productController.js
+++ controllers/productController.js
@@ -66,3 +66,27 @@ exports.filterByGender = (req, res) => {
             res.json(products);
         });
 };
+
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : 'desc';
+    let sortBy = req.body.sortBy ? req.body.sortBy : '_id';
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let findArgs = {};
+
+    // console.log(order, sortBy, limit, skip, req.body.filters);
+    // console.log("findArgs", findArgs);
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === 'price') {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
--- routes/product.js
+++ routes/product.js
@@ -6,12 +6,14 @@ const {
     list,
     filters,
     filterByGender,
+    listBySearch
 }=require("../controllers/productController");
 
 router.post("/product/create", create);
 router.get("/products", list);
 router.get("/filters",filters)
 router.get("/products/filterByGender", filterByGender);
+router.post("/products/by/search", listBySearch);
 
 
 module.exports = router;
